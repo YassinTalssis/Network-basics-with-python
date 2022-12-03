5e847a617d14b60368743ed5d867c3a116bee5f6
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2022-12-03 20:18:04 +0100

    Update TCP_Client.py

--- TCP_Client.py
+++ TCP_Client.py
@@ -5,7 +5,7 @@ PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
+    s.send(b"Hello, world")
     data = s.recv(1024)
 
 print(f"Received {data!r}")
